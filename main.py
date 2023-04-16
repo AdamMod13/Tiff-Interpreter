0dce9e03e171d07d411bb33410e4fde058107fda
Modlich Adam <adam.modlich13@gmail.com>
2023-04-16 14:29:47 +0200

    TI-2: Added masking personal info and Fourier Transformation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from PIL import Image
+import cv2
+import numpy as np
 from PIL.TiffTags import TAGS
 import tifffile as tiff
 import matplotlib.pyplot as plt
+import pytesseract
+
 
 def printDefaultTags(img):
    for tag in img.pages[0].tags.values():
@@ -35,8 +39,39 @@ def getColoursPaletteTo8Bits(img):
   palette = img.getpalette()
   print(palette)
 
-with tiff.TiffFile('file.tiff') as img:
-  printDefaultTags(img)
+def doFourierTransformation():
+  img = cv2.imread('file.tiff', 0)
+  
+  f = np.fft.fft2(img)
+  f_shift = np.fft.fftshift(f)
+
+  magnitude_spec = 20*np.log(np.absolute(f_shift))
+
+  magnitude_spec = cv2.normalize(magnitude_spec, None, 0, 255, cv2.NORM_MINMAX)
+
+  cv2.imshow('Obraz oryginalny', img)
+  cv2.imshow("Image after TF", magnitude_spec.astype('uint8'))
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+
+def maskTiffPersonalInfo(img):
+  tags_to_anonymize = [270, 271, 272, 305, 306, 315, 316]
+  tags = img.tag_v2
+    
+  for tag in tags.keys():
+    if tag in tags_to_anonymize:
+      tags[tag] = ""
+    
+  with Image.fromarray(np.array(img)) as im_output:
+    im_output.save('anonimizowany_plik.tiff')
+    
+
+
+with Image.open('casablanca.tiff') as img:
+  maskTiffPersonalInfo(img)
+
+# with tiff.TiffFile('file.tiff') as img:
+#   printDefaultTags(img)
 
 
 # with Image.open('file.tiff') as img:
